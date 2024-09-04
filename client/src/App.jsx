146d961caf5d608ec9f0c9a3dd9a5949226b3bcc
Admya1 <admyamaini@gmail.com>
2024-09-04 19:45:55 +0530

    Header Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import About from './Pages/About'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
-
+import Header from './components/Header'
+import ''
 export default function App() {
-  return (
+  return (   
     <BrowserRouter>
+     <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>
+    <div className='text-3xl text-red-100'>
       Home
     </div>
   )
