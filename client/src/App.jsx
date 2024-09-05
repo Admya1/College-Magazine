ad7fda429cf626b4a472361c89dc3d60cecf3286
Admya1 <admyamaini@gmail.com>
2024-09-05 10:58:34 +0530

    Create Server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import Header from './components/Header'
-import ''
+import './index.css'
 export default function App() {
   return (   
     <BrowserRouter>
