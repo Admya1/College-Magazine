d0a7d52b2a80e18e1c226340f1de22f98c900f1c
Admya1 <admyamaini@gmail.com>
2024-09-09 12:29:22 +0530

    Google Auth feature added

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "college-magazine-ae4b3.firebaseapp.com",
+  projectId: "college-magazine-ae4b3",
+  storageBucket: "college-magazine-ae4b3.appspot.com",
+  messagingSenderId: "255467178672",
+  appId: "1:255467178672:web:7293610db7b1aa89ee8975"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,7 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import OAuth from '../components/OAuth';
 export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [errorMessage, setErrorMessage] = useState(null);
@@ -87,6 +88,7 @@ export default function SignUp() {
                 'Sign Up'
               )}
             </Button>
+            <OAuth/>
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Have an account?</span>
@@ -94,6 +96,7 @@ export default function SignUp() {
               Sign In
             </Link>
           </div>
+          
           {errorMessage && (
             <Alert className='mt-5' color='failure'>
               {errorMessage}
