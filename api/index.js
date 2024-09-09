05993e21f1f7aac82c70fa9e5a82cf51eb0c528b
Admya1 <admyamaini@gmail.com>
2024-09-09 17:28:05 +0530

    Update user api route added

--- api/index.js
+++ api/index.js
@@ -3,11 +3,13 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{console.log("Database Connected")})
 .catch(err=>{console.log(err)});
 const app=express();
 app.use(express.json());
+app.use(cookieParser());
 app.listen(3000,()=>{console.log("Server is running");});
 app.use('/api/user',userRoutes);
 app.use('/api/auth',authRoutes);
